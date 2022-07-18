e5ceabdbdce1b62d576b42705d16bf5297a3ce00
Tryve <iamtryve@gmail.com>
2022-07-18 11:56:14 +0100

    About-Screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  HashRouter,
+  NavLink,
+} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Button } from 'react-bootstrap';
+import AboutScreen from './screens/AboutScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div>
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <div className="name">
+            <h2>Ayo</h2>
+          </div>
+          <div className="menu">
+            <ul className="menu-list">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/education">Education</NavLink>
+              </li>
+              <li>
+                <NavLink to="/tech">Technical Skills</NavLink>
+              </li>
+              <li>
+                <NavLink to="/program-lang">Programming Languages</NavLink>
+              </li>
+              <li>
+                <NavLink to="/portfolio">Portfolio</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+            <Button className="cv">View CV</Button>
+          </div>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<AboutScreen />} />
+            </Routes>
+          </Container>
+        </main>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Git from '../images/Git-Icon-1788C.png';
+import Graduate from '../images/SeekPng.com_convocation-cap-png_2699239.png';
+export default function AboutScreen() {
+  return (
+    <Container>
+      <div className="intro">
+        <div className="about">
+          <span>Hi! I'm</span>
+          <span>Ayomide Bamigbade</span>
+          <span>Preferred Name: Ayo</span>
+          <span>Frontend Developer, Fullstack Web Developer</span>
+          <span>
+            I am a versatile software graduate with commitment to develop
+            creative and innovative solutions.
+            <p>
+              I have gained practical and theoretical skills in both my studies
+              and my 3 years of commercial work experience including;
+            </p>
+            <ul>
+              <li>
+                Web Development - React.js, Node.js, Express.js, HTML, CSS
+              </li>
+              <li>Software Development - Java, C#, C++, C</li>
+              <li>Cloud Computing - SaaS, AWS, Firebase</li>
+              <li>Database Managment - MongoDB, MySQL</li>
+              <li>UI/UX Design - Figma, Miro</li>
+              <li>Testing and Agile Methods</li>
+              <li>Tools - GitHub, Redux, VS Code</li>
+            </ul>
+          </span>
+          <div className="icons">
+            <img src={Git} alt="GitHub"></img>
+          </div>
+        </div>
+        <div className="float">
+          <img src={Graduate} alt="Graduate Logo" />
+          <h3 className="title">Graduate Level Titles</h3>
+          <span style={{ top: '-4%', left: '68%' }}>
+            Data <br /> Analyst
+          </span>
+          <span style={{ top: '60%', right: '77%' }}>
+            UI/UX <br /> Designer
+          </span>
+          <span style={{ top: '400%', right: '60%' }}>
+            Project <br /> Manager
+          </span>
+        </div>
+      </div>
+    </Container>
+  );
+}
