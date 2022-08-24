08d648439ef87902e28c5e5e6c832ec709a2454d
Tryve <iamtryve@gmail.com>
2022-08-24 01:31:06 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -17,6 +17,16 @@ import StackScreen from './screens/StackScreen.js';
 import Wave2 from './images/Wave2.jpg';
 
 function App() {
+  const toggle = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const hamburger_icon = hamburger.querySelector('span');
+    const mobile_menu = document.querySelector('.mobile-menu');
+    hamburger.addEventListener('click', () => {
+      hamburger_icon.innerText =
+        hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
+      mobile_menu.classList.toggle('is-open');
+    });
+  };
   return (
     <HashRouter>
       <div>
@@ -43,9 +53,29 @@ function App() {
                 <NavLink to="/contact">Contact</NavLink>
               </li>
             </ul>
+            <Button className="hamburger" onClick={toggle}>
+              <span className="material-icons">menu</span>
+            </Button>
             <a href={CV} target="_blank" rel="noreferrer">
               <Button className="cv">View CV</Button>
             </a>
+            <ul className="mobile-menu">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/education">Education</NavLink>
+              </li>
+              <li>
+                <NavLink to="/program-stack">Programming Stack</NavLink>
+              </li>
+              <li>
+                <NavLink to="/portfolio">Portfolio</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
           </div>
         </header>
         <main>
